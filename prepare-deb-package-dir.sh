fd824c1bb724db347dd3261b3ba175ac1e7e3d34
Andrey Rogov <rogovandrey@eltechs.com>
2016-03-30 00:04:46 +0300

    Support build mem2g and mem3g

--- prepare-deb-package-dir.sh
+++ prepare-deb-package-dir.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-TOPDIR=$1
 WINEVERSION=1.8.1
+TOPTOPDIR=$1
 
-rm -f .gitattributes
-mkdir -p ${TOPDIR}/wine-build
-echo 'prepare-deb-package-dir.sh export-ignore' > .gitattributes
 
-git archive HEAD --format=tar --worktree-attributes -o ${TOPDIR}/wine-build/debian.tar debian
-echo 'debian export-ignore' >> .gitattributes
-git archive HEAD --format=tar.gz  --worktree-attributes -o ${TOPDIR}/wine_${WINEVERSION}.orig.tar.gz 
 
-rm -f .gitattributes
 
-tar -xf ${TOPDIR}/wine-build/debian.tar --directory=${TOPDIR}/wine-build
-tar -xzf ${TOPDIR}/wine_${WINEVERSION}.orig.tar.gz --directory=${TOPDIR}/wine-build/
-rm ${TOPDIR}/wine-build/debian.tar
+function create_build_dir
+{
+    rm -f .gitattributes
+    
+    TOPDIR=${TOPTOPDIR}/${MEMSPLIT}
+    DEBIANDIR="debian-${MEMSPLIT}"
+
+    mkdir -p ${TOPDIR}/wine-build
+    echo 'prepare-deb-package-dir.sh export-ignore' > .gitattributes
+
+    git archive HEAD --format=tar --worktree-attributes -o ${TOPDIR}/wine-build/debian.tar ${DEBIANDIR}
+    echo "debian-mem2g export-ignore" >> .gitattributes
+    echo "debian-mem3g export-ignore" >> .gitattributes    
+    git archive HEAD --format=tar.gz  --worktree-attributes -o ${TOPDIR}/wine_${WINEVERSION}.orig.tar.gz 
+
+    rm -rf .gitattributes
+
+    tar -xf ${TOPDIR}/wine-build/debian.tar --directory=${TOPDIR}/wine-build
+    mv ${TOPDIR}/wine-build/${DEBIANDIR} ${TOPDIR}/wine-build/debian
+    tar -xzf ${TOPDIR}/wine_${WINEVERSION}.orig.tar.gz --directory=${TOPDIR}/wine-build/
+    
+    rm ${TOPDIR}/wine-build/debian.tar
+}
+
+MEMSPLIT='mem2g'
+create_build_dir
+MEMSPLIT='mem3g'
+create_build_dir
 
 
 
